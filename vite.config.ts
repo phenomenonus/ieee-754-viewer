6afec2db2240639b6240b138be22b3b07fa9663e
Mikhail Prugov <mikhail.prugov@gmail.com>
2026-07-15 13:03:33 +0300

    fix(vite): set vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ieee-754-viewer/",
   build: {
     rolldownOptions: {
       output: {
